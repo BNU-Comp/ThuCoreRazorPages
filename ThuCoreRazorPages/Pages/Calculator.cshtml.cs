4b461cfef8ca59c12b5f9c9e7fc4e0bc48f6c821
DerekPeacock <derek.peacock@yahoo.com>
2019-10-13 14:31:57 +0100

    Added footer, Hello and Calculator pages

---
+++ ThuCoreRazorPages/Pages/Calculator.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ThuCoreRazorPages.Pages
+{
+    public class CalculatorModel : PageModel
+    {
+        public Decimal Pounds { get; set; }
+
+        public Decimal Euros { get; set; }
+
+        public CultureInfo Portugal = new CultureInfo("pt-PT");
+
+        public string currencyFormat = "C2";
+
+        public void OnGet()
+        {
+            Pounds = 0m;
+            Euros = 0m;
+        }
+
+
+        public void OnPost()
+        {
+            string value = Request.Form["Pounds"];
+            Pounds = Convert.ToDecimal(value);
+
+            Euros = Pounds * 1.12m;
+        }
+    }
+}
---
+++ ThuCoreRazorPages/Pages/Hello.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ThuCoreRazorPages.Pages
+{
+    public class HelloModel : PageModel
+    {
+        public String name;
+
+        /// <summary>
+        /// This method is called when the web page loads
+        /// and sets the name to nothing
+        /// </summary>
+        public void OnGet()
+        {
+            name = null;
+        }
+
+        /// <summary>
+        /// This method is called when the user clicks
+        /// on the submit button and it retreives the
+        /// name from the form and stores it in the
+        /// Model properties
+        /// </summary>
+        public void OnPost()
+        {
+            this.name = Request.Form["name"];
+        }
+    }
+}
